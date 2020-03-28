25d153e8f496c79a91e0e6fd752037f94eae7b60
Ricardo Casares <ricardocasares@gmail.com>
2020-03-28 19:19:57 +0100

    fix: update manifest

--- .config/manifest.js
+++ .config/manifest.js
@@ -2,44 +2,29 @@ module.exports = {
   output: "./public/",
   name: "Tootai",
   short_name: "Tootai",
-  display: "standalone",
-  theme_color: "#000000",
   start_url: "/?utm_source=pwa",
+  display: "standalone",
+  theme_color: "#4c6ef5",
   background_color: "#ffffff",
   icons: [
     {
-      src: "/icon-512x512.png",
+      src: "/icons/512x512.png",
       sizes: "512x512",
       type: "image/png"
     },
     {
-      src: "/icon-384x384.png",
-      sizes: "384x384",
-      type: "image/png"
-    },
-    {
-      src: "/icon-192x192.png",
+      src: "/icons/192x192.png",
       sizes: "192x192",
       type: "image/png"
     },
     {
-      src: "/icon-192x192.png",
-      sizes: "192x192",
+      src: "/icons/32x32.png",
+      sizes: "32x32",
       type: "image/png"
     },
     {
-      src: "/icon-152x152.png",
-      sizes: "152x152",
-      type: "image/png"
-    },
-    {
-      src: "/icon-96x96.png",
-      sizes: "96x96",
-      type: "image/png"
-    },
-    {
-      src: "/icon-72x72.png",
-      sizes: "72x72",
+      src: "/icons/16x16.png",
+      sizes: "16x16",
       type: "image/png"
     }
   ]
