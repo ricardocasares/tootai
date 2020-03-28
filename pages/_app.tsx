cf83aad838fb37f4351ebb8a4a87da35cabed295
Ricardo Casares <ricardocasares@gmail.com>
2020-03-28 19:11:17 +0100

    feat: update icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,9 @@ export default ({ Component, pageProps }: AppProps) => (
     <Head>
       <title>tootai</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/icons/16x16.png" />
       <link rel="manifest" href="/manifest.json" />
     </Head>
     <Component {...pageProps} />
